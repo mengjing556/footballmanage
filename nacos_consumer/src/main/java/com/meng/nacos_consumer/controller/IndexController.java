1a3850a0c54fbe4b7fdae44432d4d5b24d0e2f1b
mengjingyu <2454473419@qq.com>
2022-07-31 17:38:52 +0800

    修改readme并添加docx文档存放运行截图。

--- nacos_consumer/src/main/java/com/meng/nacos_consumer/controller/IndexController.java
+++ nacos_consumer/src/main/java/com/meng/nacos_consumer/controller/IndexController.java
@@ -1,14 +1,12 @@
 package com.meng.nacos_consumer.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @RequestMapping("/index")
-    public String index(Model model){
-        model.addAttribute("data", "SpringBoot框架集成jsp页面");
+    public String index(){
         return "index";
     }
 }
